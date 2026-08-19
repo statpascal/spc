666026893097dab2bb2629861bde22654c193e8d
statpascal <git@greylags.de>
2026-08-19 20:53:03 +0200

    Adjusted pathes to emul99 0.2.3

--- scripts/runbank.sh
+++ scripts/runbank.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+. $(dirname "$0")/setpath.sh
 obj/sp $1
 rm cart_b*.bin cart.bin
-~/ti99/xdt99/xas99.py -R -b -q -L out.lst out.a99 -o cart.bin
+$XAS99 -R -b -q -L out.lst out.a99 -o cart.bin
 cat cart_b*.bin >>cart.bin
-~/ti99/emul99/bin/emul99 scripts/emul99.cfg cart_rom=cart.bin $2
+$EMUL99 scripts/emul99.cfg cart_rom=cart.bin $2
--- scripts/runcart.sh
+++ scripts/runcart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. $(dirname "$0")/setpath.sh
 obj/sp --cart $1
-~/ti99/xdt99/xas99.py -R -b -q -L out.lst out.a99 -o cart.bin
-~/ti99/emul99/bin/emul99 ~/ti99/emul99/bin/common.cfg mem_ext=1 disksim_dsr=~/ti99/emul99/roms/disksim.bin disksim_dir=~/src/statpascal cart_rom=cart.bin disksim_text=1 $2
+$XAS99 -R -b -q -L out.lst out.a99 -o cart.bin
+$EMUL99 scripts/emul99.cfg cart_rom=cart.bin disksim_text=1
