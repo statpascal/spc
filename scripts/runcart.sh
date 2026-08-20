d25b7cf964aa80469e9c1b5db0ba06056cabb9f9
statpascal <git@greylags.de>
2026-08-20 14:44:10 +0200

    Added cfg for emul99

--- scripts/runcart.sh
+++ scripts/runcart.sh
@@ -2,4 +2,4 @@
 . $(dirname "$0")/setpath.sh
 obj/sp --cart $1
 $XAS99 -R -b -q -L out.lst out.a99 -o cart.bin
-$EMUL99 scripts/emul99.cfg cart_rom=cart.bin disksim_text=1
+$EMUL99 scripts/emul99.cfg cart_rom=cart.bin $2
--- scripts/setpath.sh
+++ scripts/setpath.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+in/bash
 EMUL99=../emul99/bin/emul99
 XAS99=../xdt99/xas99.py
 XDM99=../xdt99/xdm99.py
